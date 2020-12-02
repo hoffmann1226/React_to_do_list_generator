03b3a4624fe369254742e45fa24de19f21223253
Tom Hoffmann <64169852+hoffmann1226@users.noreply.github.com>
2020-12-02 10:47:44 -0600

    database created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,8 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
+          <h1 className="App-title">To Do List Generator</h1>
         </header>
         <br/>
       </div>
